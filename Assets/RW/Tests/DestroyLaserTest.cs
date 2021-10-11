3381c79e56be3077bbc162fd2687e16400232dd1
AllexisAlvarico <C00242855@itcarlow.ie>
2021-10-11 11:28:14 +0100

    The laser destroy test works
    Improved the testing on laser destroy
    
    “Implementation and tests for Laser Destroys Asteroid task"

---
+++ Assets/RW/Tests/DestroyLaserTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class DestroyLaserTest
+    {
+        private Game game;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject =
+            MonoBehaviour.Instantiate(
+                Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Game>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(game.gameObject);
+        }
+
+        [UnityTest]
+        public IEnumerator LaserDestroysAsteroid()
+        {
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = Vector3.zero;
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = Vector3.zero;
+            yield return new WaitForSeconds(0.1f);
+            UnityEngine.Assertions.Assert.IsNull(asteroid);
+        }
+
+    }
+}
